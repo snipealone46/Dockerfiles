e2e4cf2706d8aedd7168205cf1bed917c6283878
Eric Yang <eric.shaohuiyang@gmail.com>
2016-06-29 23:33:53 -0700

    add default branch and rails_env

--- dockerfiles/to-do-list/entrypoint.sh
+++ dockerfiles/to-do-list/entrypoint.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-/bin/bash
+if [ -z "$RAILS_ENV" ]; then
+  RAILS_ENV=development
+fi
+
+if [ -z "$BRANCH" ]; then
+  BRANCH=dev
+fi
+
+if [ $# -eq 0]; then
+  /bin/bash
+else
+  exec "$@"
+fi

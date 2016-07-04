27e099556b494923437eab6c1f750016a14f5611
Eric Yang <eric.shaohuiyang@gmail.com>
2016-07-04 10:12:41 -0700

    add container init lock feature so that container will only initialize once even restarted

--- dockerfiles/to-do-list/entrypoint.sh
+++ dockerfiles/to-do-list/entrypoint.sh
@@ -10,6 +10,20 @@ if [ -z "$BRANCH" ]; then
   BRANCH=dev
 fi
 
+init_lock="/var/lock/container-init"
+
+# Create lock file for init process
+if [ ! -f $init_lock/initialize ]; then
+    # Create a empty file when container initial setup
+    init_lock=$init_lock /docker-scripts/setup_init_lock.sh
+
+    # Setup rails app
+    cd /opt/$APPLICATION/current
+    git pull && git checkout $BRANCH
+    bundle install
+
+fi
+
 if [ $# -eq 0 ]; then
   /bin/bash
 else
